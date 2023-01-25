b551ca1f9d164737b59803c947eb14aa583da28a
CimarSantos <francimarlsantos@outlook.com>
2023-01-25 17:53:33 -0300

    Feat: Conexão com o DB na nuvem

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.get("/", async (req, res) => {
+  const actionGames = await db.collection("actionGames").find();
+  res.send(actionGames);
+});
+
 server.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
+  console.log(`Server is listening on port 5000`);
 });
