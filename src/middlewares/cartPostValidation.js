c7acd4d6b385e7928a320f00c47629524d945ba6
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-01-28 17:23:45 -0300

    refactor: Ajuste na Rota POST cart

---
+++ src/middlewares/cartPostValidation.js
@@ -0,0 +1,22 @@
+import { db } from "../database/db.js";
+import { COLLECTION } from "../enums/collections.js";
+import { STATUS_CODE } from "../enums/statusCode.js";
+
+export async function cartPostValidation(req, res, next) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+
+    try {
+        if (!token) return res.sendStatus(STATUS_CODE.NOT_FOUND);
+        const user = await db.collection(COLLECTION.SESSION).findOne({ token });
+        if (!user) return res.sendStatus(STATUS_CODE.UNAUTHORIZED);
+
+        res.locals.user = user;
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.status(STATUS_CODE.SERVER_ERROR).send(error);
+    } s;
+}
