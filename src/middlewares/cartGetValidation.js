7c96b85232d4d59d40b4835486217e54d72af33d
RubsRafa <rubiarafaela2000@gmail.com>
2023-01-27 21:55:19 -0300

    feat: controller, validation, routes

---
+++ src/middlewares/cartGetValidation.js
@@ -0,0 +1,23 @@
+import { db } from "../database/db.js";
+import { COLLECTION } from "../enums/collections.js";
+import { STATUS_CODE } from "../enums/statusCode.js";
+
+export async function cartGetValidation(req, res, next) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+
+    try {
+
+        if (!token) return res.sendStatus(STATUS_CODE.UNAUTHORIZED);
+        const user = await db.collection(COLLECTION.SESSION).findOne({ token }).toArray();
+        if (!user) return res.sendStatus(STATUS_CODE.NOT_FOUND);
+
+        res.locals.user = user;
+
+        next(); 
+
+    } catch (error) {
+        console.log(error);
+        return res.status(STATUS_CODE.SERVER_ERROR).send(error);
+    }
+}
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { userRouter } from "./routes/userRouter.js";
 import { authRouter } from "./routes/authRouter.js";
+import { cartRouter } from './routes/cartRouter.js';
 
 const server = express();
 server.use(express.json());
@@ -9,6 +10,7 @@ server.use(cors());
 
 server.use(userRouter);
 server.use(authRouter); 
+server.use(cartRouter);
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is listening on port 5000`);
