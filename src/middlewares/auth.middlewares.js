0af664278c9a2e4eb6649dba9d60cad2791b49c0
RubsRafa <rubiarafaela2000@gmail.com>
2023-01-30 11:52:44 -0300

    test: enter

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -4,14 +4,18 @@ import { COLLECTION } from "../enums/collections.js";
 import { db } from "../database/db.js";
 
 function validadeSignUp(req, res, next) {
-  const user = req.body;
-  const { error } = signUpSchema.validate(user, { abortEarly: false });
+  const {
+    name,
+    email,
+    image,
+    password,
+    confirmPassword
+  } = req.body;
+  const { error } = signUpSchema.validate({name, email, image, password, confirmPassword}, { abortEarly: false });
 
   if (error) {
     const message = error.details
       .map((detail) => detail.message)
-      .join(",")
-      .replace("[ref:password]", "equal to password");
     return res.status(STATUS_CODE.UNPROCESSABLE_ENTITY).send({ message });
   }
   res.locals.user = user;
